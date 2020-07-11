6067b4422fb988febf22b396f45c89b59446ba17
DarkinngxD <darkinng123@gmail.com>
2020-07-11 16:25:37 -0500

    Graficos Android
    Se agrego grafico de pastel

--- AppMovil/app/src/main/java/com/example/navigationdrawerpractica/Interfaces/MainActivity.java
+++ AppMovil/app/src/main/java/com/example/navigationdrawerpractica/Interfaces/MainActivity.java
@@ -15,19 +15,32 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.navigationdrawerpractica.Entidades.Persona;
 import com.example.navigationdrawerpractica.Fragments.DetallePersonaFragment;
 import com.example.navigationdrawerpractica.Fragments.Detalle_HistorialFragment;
+import com.example.navigationdrawerpractica.Fragments.GraficoFragment;
 import com.example.navigationdrawerpractica.Fragments.HistorailFragment;
 import com.example.navigationdrawerpractica.Fragments.MainFragment;
 import com.example.navigationdrawerpractica.Fragments.PersonasFragment;
 import com.example.navigationdrawerpractica.R;
 import com.google.android.material.navigation.NavigationView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, iComunicaFragments{
 
     DrawerLayout drawerLayout;
+    private RequestQueue mQueue;
     ActionBarDrawerToggle actionBarDrawerToggle;
     Toolbar toolbar;
     NavigationView navigationView;
@@ -36,16 +49,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     FragmentTransaction fragmentTransaction;
     //variable del fragment detalle
     DetallePersonaFragment detallePersonaFragment;
+    TextView tvDatTotCom,tvDatTotPen ;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         drawerLayout = findViewById(R.id.drawer);
         navigationView = findViewById(R.id.navigationView);
+        tvDatTotPen  =findViewById(R.id.tvDatTotPen);
+        tvDatTotCom=findViewById(R.id.tvDatTotCom);
         //lo sgt se implementa luego de haber implementado NavigationView.OnNavigationItemSelectedListener
         navigationView.setNavigationItemSelectedListener(this);
 
@@ -59,6 +76,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.container_fragment,new MainFragment());
         fragmentTransaction.commit();
+        mQueue = Volley.newRequestQueue(this);
+        fillLista();
+        fillLista2();
 
 
     }
@@ -75,6 +95,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         //para cerrar automaticamente el menu
+
+
+        SharedPreferences.Editor editor = getSharedPreferences("gymapp", Context.MODE_PRIVATE).edit();
         drawerLayout.closeDrawer(GravityCompat.START);
         if(menuItem.getItemId() == R.id.home){
             fragmentManager = getSupportFragmentManager();
@@ -97,13 +120,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         if(menuItem.getItemId() == R.id.home2){
             fragmentManager = getSupportFragmentManager();
             fragmentTransaction = fragmentManager.beginTransaction();
+            editor.putString("completos",tvDatTotCom.getText().toString());
+            editor.putString("pendientes",tvDatTotPen.getText().toString());
+            editor.commit();
+            fragmentTransaction.replace(R.id.container_fragment,new GraficoFragment());
+            fragmentTransaction.commit();
+        }
+        if(menuItem.getItemId() == R.id.home3){
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
             fragmentTransaction.replace(R.id.container_fragment,new Detalle_HistorialFragment());
             fragmentTransaction.commit();
         }
         if(menuItem.getItemId() == R.id.personas2){
-//            Intent encu2 = new Intent(this, Login.class);
-//            startActivity(encu2);
-            SharedPreferences.Editor editor = getSharedPreferences("gymapp", Context.MODE_PRIVATE).edit();
             editor.clear();
             editor.commit();
             startActivity(new Intent(MainActivity.this, Login.class));
@@ -140,7 +169,53 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         //***Luego pasar a programar al fragmentdetalle
     }
 
+    private void fillLista() {
+        String url = "http://dgform.ga/forms/complete";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("data");
+                            tvDatTotCom.setText(String.valueOf(jsonArray.length()));
 
+                        } catch (JSONException ex) {
+                            ex.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
+    }
+
+    private void fillLista2() {
+        String url2 = "http://dgform.ga/forms/pending";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url2, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("data");
+                            tvDatTotPen.setText(String.valueOf(jsonArray.length()));
+                            // pendientes =jsonArray.length();
+                        } catch (JSONException ex) {
+                            ex.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
+    }
 
 
 }
