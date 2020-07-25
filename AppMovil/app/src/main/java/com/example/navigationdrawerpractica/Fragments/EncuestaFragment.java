e11ed7592e30822d567cae18c80d557e23f125c2
yonattan95 <30610266+yonattan95@users.noreply.github.com>
2020-07-24 23:43:12 -0500

    Revert "Jferro"

--- AppMovil/app/src/main/java/com/example/navigationdrawerpractica/Fragments/EncuestaFragment.java
+++ AppMovil/app/src/main/java/com/example/navigationdrawerpractica/Fragments/EncuestaFragment.java
@@ -9,7 +9,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
