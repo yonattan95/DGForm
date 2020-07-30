597794ad70875e9a2d0a9e313349f26821413771
DarkinngxD <darkinng123@gmail.com>
2020-07-30 02:40:27 -0500

    borrar archivo

--- AppMovil/app/src/main/java/com/example/digiforms/Interfaces/iComunicaFragments.java
+++
@@ -1,9 +0,0 @@
-package com.example.digiforms.Interfaces;
-
-import com.example.digiforms.Entidades.Persona;
-
-public interface iComunicaFragments {
-    //esta interface se encarga de realizar la comunicacion entre la lista de personas y el detalle
-    public void enviarPersona(Persona persona); //se transportara un objeto de tipo persona
-    //(En la clase Persona se implementa Serializable para poder transportar un objeteo a otro)
-}
--- AppMovil/app/src/test/java/com/example/digiforms/Fragments/PersonasFragmentTest.java
+++
@@ -1,9 +0,0 @@
-package com.example.digiforms.Fragments;
-
-class EncuestaFragmentTest {
-
-    @org.junit.jupiter.api.Test
-    void mostrarData() {
-
-    }
-}
